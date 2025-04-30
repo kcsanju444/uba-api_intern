ab47fc1d788ae840d30b77a40b01a850e4006848
kcsanju444 <kcsanju600@gmaill.com>
2025-04-30 17:46:54 +0545

    move MySQL connection details to .env file

--- src/graphql/db.ts
+++ src/graphql/db.ts
@@ -1,18 +1,21 @@
 import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "employeedb"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-con.connect(function(err) {
-    if (err) {
-        console.log("Connection error:", err);
-    } else {
-        console.log("Connected to MySQL database");
-    }
+con.connect((err) => {
+  if (err) {
+    console.log('Connection error:', err);
+  } else {
+    console.log('Connected to MySQL database');
+  }
 });
 
 export default con;
